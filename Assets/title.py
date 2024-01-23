4a2195bd65e3cbec7c385f8de1f1adea19398355
Jaya Winata <jayawinata100@gmail.com>
2024-01-23 11:21:37 +0700

    stats page layout

--- Assets/title.py
+++ Assets/title.py
@@ -1,5 +1,8 @@
 import customtkinter as ctk
 
 class Title(ctk.CTkLabel):
-    def __init__(self,text,anchor,master):
+    def __init__(self,master,text='',anchor='w'):
         super().__init__(master=master,text=text,font=('Bahnschrift SemiBold',20,'bold'),anchor=anchor)
+
+    def getFont(self):
+        return self._font
--- Pages/pages.py
+++ Pages/pages.py
@@ -32,9 +32,43 @@ class Dashboard(ctk.CTkFrame):
         
         self.plot_frame = ctk.CTkFrame(self,height=(master.winfo_height()/3))
         self.plot_frame.pack_configure(fill='x',side='top',padx=10,pady=10)
+        self.plot_frame.bind('<Button-1>',lambda x: self.stats(master))
 
         self.schedule_frame = ctk.CTkFrame(self,width=(master.winfo_width()/2-10))
         self.schedule_frame.pack_configure(fill='y',side='left',padx=10,pady=10)
 
         self.report_frame = ctk.CTkFrame(self,width=(master.winfo_width()/2-10))
         self.report_frame.pack_configure(fill='y',side='left',padx=10,pady=10)
+
+    def render_plot():
+        pass
+
+    def stats(self,master):
+        self.destroy()
+        Stats(master)
+
+class Stats(ctk.CTkScrollableFrame):
+    def __init__(self,master: ctk.CTk):
+        super().__init__(master)
+        self.pack(expand=1,fill='both',padx=10,pady=10)
+        self.configure(fg_color='transparent')
+        self.title = Title(self,text='Stats')
+        self.title.pack_configure(fill='x',side='top',padx=20)
+
+        self.body_weight_frame = ctk.CTkFrame(self,height=(master.winfo_height() / 3 -10))
+        self.body_weight_frame.pack_configure(fill='x',side='top',padx=10,pady=10)
+        
+        self.calories_frame = ctk.CTkFrame(self,height=(master.winfo_height() / 3 -10))
+        self.calories_frame.pack_configure(fill='x',side='top',padx=10,pady=10)
+        
+        self.body_fat_frame = ctk.CTkFrame(self,height=(master.winfo_height() / 3 -10))
+        self.body_fat_frame.pack_configure(fill='x',side='top',padx=10,pady=10)
+
+        self.back_lable = ctk.CTkLabel(master=self,text='<< Back',anchor='w',text_color='#5f5f5f')
+        self.back_lable.pack_configure(padx=20,pady=10,fill='x',side='top')
+        self.back_lable.bind('<Button-1>',lambda x: self.back(master))
+
+    def back(self,master):
+        self.pack_forget()
+        self.destroy()
+        Dashboard(master)
